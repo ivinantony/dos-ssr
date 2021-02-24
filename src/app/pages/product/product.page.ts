dfec174ba9851ec1675b7636a71c6354bf5d7b0e
matty1203 <mathews.philip@mermerapps.com>
2021-02-24 18:35:18 +0530

    add to cart fix

--- src/app/pages/product/product.page.ts
+++ src/app/pages/product/product.page.ts
@@ -227,7 +227,7 @@ export class ProductPage implements OnInit {
           client_id: val,
           qty: this.qty,
         };
-        this.cartService.addToCart(data).subscribe(
+        this.cartService.addToCartQty(data).subscribe(
           (data) => this.handleResponse(data, POST_DATA),
           (error) => this.handleError(error)
         );
